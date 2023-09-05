63b8bfdbdfcd68ca11c57d0706f102df74df2540
Matthew <matthewhiggins.fs3@gmail.com>
2023-09-05 18:47:27 +0200

    Added docstring for the module in module.py

---
+++ module.py
@@ -0,0 +1,10 @@
+
+"""This module contains utility functions for data manipulation."""
+
+def function1():
+    """This is the first utility function."""
+    pass
+
+def function2():
+    """This is the second utility function."""
+    pass
