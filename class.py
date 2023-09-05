e33b015d288653c036965700051013ab69c4dbbf
Matthew <matthewhiggins.fs3@gmail.com>
2023-09-05 18:46:24 +0200

    Added docsting for class in class.py

---
+++ class.py
@@ -0,0 +1,18 @@
+class Person:
+    """Represents a person.
+
+    Attributes:
+        name (str): The person's name.
+        age (int): The person's age.
+    """
+
+    def __init__(self, name, age):
+        """
+        Initialize a new Person object.
+
+        Args:
+            name (str): The person's name.
+            age (int): The person's age.
+        """
+        self.name = name
+        self.age = age
